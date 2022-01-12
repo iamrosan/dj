e1856667ad875586432e149ca624f4f0918d72da
iamrosan <roshanshrestha21.63@gmail.com>
2022-01-12 10:38:04 +0545

    D2

--- base/views.py
+++ base/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+rooms =[
+         {'id':1,'name':"Learn Python"},
+         {'id':2,'name':"Learn Javascript"},
+         {'id':3,'name':"Learn Data Science"}
+        ]
+
+
+
 def index(request):
-    return render(request,'base/index.html')
+    context = {'rooms':rooms}
+    return render(request,'base/index.html',context)
 
 def pricing(request):
     return render(request,'base/pricing.html')
+
+def room(request,pk):
+    room =None
+    for i in rooms:
+        if(i['id'] == int(pk)):
+            room=i
+    context = {'room':room}
+    return render(request,'base/rooms.html',context)
